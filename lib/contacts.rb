6cc6fcb6ba68745e9f4317f2ae3279ff4083e72e
Stephen Olson <stephenolson@gmail.com>
2018-10-11 17:33:52 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,7 +15,6 @@ require 'pry'
    }
   
 def remove_strawberry(contacts)
-  def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     contact_details_hash.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
@@ -23,6 +22,4 @@ def remove_strawberry(contacts)
       end
     end
   end
-end
-
 end
